80487f1c5a3b6f79609f11277efc72a15c5555db
Swirl86 <swesuzzi@gmail.com>
2021-06-02 20:57:29 +0200

    Shake Acceleration task

---
+++ Uppgift 1/ShakeAcceleration/app/src/main/java/com/example/shakeacceleration/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.shakeacceleration;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private final float SHAKE_THRESHOLD = 1.2f;
+    private final int SHAKE_TIMEOUT_MS = 500;
+
+    private MainFragment accelerometer;
+    private FragmentContainerView  fragmentContainerView;
+    private TextView textView;
+    private long rotatedTimeStamp = 0;
+
+    static Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        accelerometer = new MainFragment(this);
+        fragmentContainerView = (FragmentContainerView) findViewById(R.id.id_fragment_container_view);
+        textView = (TextView) findViewById(R.id.id_info_text_view);
+
+        context = getApplicationContext();
+
+        accelerometer.setListener(new MainFragment.Listener() {
+            @Override
+            public void onTranslation(float x, float y, float z) {
+                double gForce = getForce(x, y, z);
+                long  currentTime = System.currentTimeMillis();
+                textView.setBackgroundColor(Color.RED);
+                textView.setText("X: " + x + "\nY: " + y + "\nZ: " + z + "\nG-force: " + gForce);
+
+                if(gForce > SHAKE_THRESHOLD){
+                    if(rotatedTimeStamp + SHAKE_TIMEOUT_MS < currentTime) {
+                        textView.setBackgroundColor(Color.GREEN);
+                        fragmentContainerView.setRotation(fragmentContainerView.getRotation() + 90);
+                        rotatedTimeStamp = currentTime;
+                    }
+                }
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        accelerometer.register();
+    }
+
+    @Override
+    protected void onPause() {
+        accelerometer.unregister();
+        super.onPause();
+    }
+
+    private double getForce(double x, double y, double z) {
+        double gX = x / SensorManager.GRAVITY_EARTH;
+        double gY = y / SensorManager.GRAVITY_EARTH;
+        double gZ = z / SensorManager.GRAVITY_EARTH;
+        return Math.sqrt((double) (gX * gX + gY * gY + gZ * gZ));
+    }
+}
---
+++ Uppgift 1/ShakeAcceleration/app/src/main/java/com/example/shakeacceleration/MainFragment.java
@@ -0,0 +1,71 @@
+package com.example.shakeacceleration;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public interface Listener {
+        void onTranslation(float tx, float ty, float tz);
+    }
+
+    private Listener listener;
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private SensorEventListener sensorEventListener;
+
+    public MainFragment() {
+        // Required empty public constructor
+       // Log.d("myTag", "Fragment Starting!");
+
+    }
+
+    public MainFragment(Context context) {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if(listener != null) {
+                    listener.onTranslation(event.values[0], event.values[1], event.values[2]);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
+    public void register(){
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void unregister(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
